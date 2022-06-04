fd843192f04eb7cf6660e9466148825e303bc2bd
Avery Kyle Arenas <kyruken@gmail.com>
2022-06-04 15:39:13 -0700

    fix form and adds books

--- javascript.js
+++ javascript.js
@@ -1,13 +1,16 @@
 
 //array to store the books
+const theForm = document.getElementById('form');
 let myLibrary = [];
+let libraryIndex = 0;
 
 function openForm() {
-    document.getElementById('form').style.display = "block";
+    document.getElementById('form-container').style.display = "block";
 }
 
 function closeForm() {
-    document.getElementById('form').style.display = "none";
+    document.getElementById('form-container').style.display = "none";
+    addBooktoLibrary();
 }
 
 function Book(title, author, pages, haveRead) {
@@ -21,28 +24,19 @@ function Book(title, author, pages, haveRead) {
         return `${title} by ${author}, ${pages}, ${haveRead}`;
     }
 
-
 }
 
 function addBooktoLibrary() {
     //adding book function
-    let title = prompt("What's the title?");
-    let author = prompt("Who's the author?");
-    let pages = prompt("How many pages are there?");
-    let haveRead = prompt("Have you read it?");
-
-    let libraryIndex = 0;
-
-    let newBook = new Book(title, author, pages, haveRead);
+    let newBook = new Book(form.title.value, form.author.value, form.pages.value, form.read.value);
 
     myLibrary[libraryIndex] = newBook;
-    libraryIndex++;
-
+    addBooktoDOM();
 }
 
 function addBooktoDOM() {
     const container = document.querySelector('.cards-list');
-    for (let x = 0; x < myLibrary.length; x++) {
+    for (let x = libraryIndex; x < myLibrary.length; x++) {
         let newDiv = document.createElement('div');
         let newTitle = document.createElement('h3');
         let newAuthor = document.createElement('p');
@@ -53,6 +47,9 @@ function addBooktoDOM() {
         newAuthor.textContent = `Author: ${myLibrary[x].author}`;
         newPages.textContent = `Pages: ${myLibrary[x].pages}`;
         newHaveRead.textContent = `Have read: ${myLibrary[x].haveRead}`;
+        
+        newDiv.setAttribute('id', `book-${x}`);
+        console.log(newDiv.id);
 
         newDiv.appendChild(newTitle);
         newDiv.appendChild(newAuthor);
@@ -62,6 +59,7 @@ function addBooktoDOM() {
         newDiv.classList.add('card');
 
         container.appendChild(newDiv);
+        libraryIndex++;
 
     }
 }
