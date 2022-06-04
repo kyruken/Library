eb4e4794396ea2da1a4f2179604a1b9f45d78144
Avery Kyle Arenas <kyruken@gmail.com>
2022-06-04 12:32:08 -0700

    add book object

--- javascript.js
+++ javascript.js
@@ -2,8 +2,18 @@
 //array to store the books
 let myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, haveRead) {
     //the constructor
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+
+    this.info = function() {
+        return `${title} by ${author}, ${pages}, ${haveRead}`;
+    }
+
+
 }
 
 function addBooktoLibrary() {
